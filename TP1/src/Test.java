90c58b03fcd7fac7ed5f21ee863477d1b2857686
HardHuino <156691992+HardHuino@users.noreply.github.com>
2025-11-11 12:55:29 +0100

    Removed "out" from root

---
+++ TP1/src/Test.java
@@ -0,0 +1,8 @@
+import com.feteforraine.Autotamponneuses;
+
+public class Test {
+    public static void main(String[] args) {
+        Autotamponneuses a = new Autotamponneuses();
+        a.ajouteOccupant("Henri");
+    }
+}
